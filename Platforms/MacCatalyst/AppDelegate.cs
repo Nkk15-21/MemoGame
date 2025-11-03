db0f37e56c3eb6ac111e382b47f6515dfc73c31b
opilane <opilane@HP-CZC2349HTN>
2025-11-03 09:57:59 +0200

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MemoGame
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
